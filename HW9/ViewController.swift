1e3f3aa275acd43b5d467005679f396e3c9c8c72
kramarkirill <kkramar@kantiana.ru>
2020-06-16 20:02:54 +0200

    Initial Commit

---
+++ HW9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW9
+//
+//  Created by Кирилл Крамар on 16.06.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
